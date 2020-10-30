170042db74d167dda3c9daa67c0be01d923cec23
wukong-xxs <wukong.xxs@gmail.com>
2020-10-30 16:42:33 +0800

    Modify

--- solidity/contracts/wrapperproxy/RBTCWrapperProxy.sol
+++ solidity/contracts/wrapperproxy/RBTCWrapperProxy.sol
@@ -12,17 +12,59 @@ contract RBTCWrapperProxy{
     using Address for address;
 
     IWrbtcERC20 wrbtc;
-    ILiquidityPoolV2Converter liquidityPoolV2Converter;
+    ILiquidityPoolV2Converter liquidityPoolConverter;
     ISmartToken poolToken;
     ISovrynSwapNetwork sovrynSwapNetwork;
-    IERC20Token docToken;
+    IERC20Token token;
     
     address public wrbtcTokenAddress;
-    address public liquidityPoolV2ConverterAddress;
+    address public liquidityPoolConverterAddress;
     address public poolTokenAddress;
     address public sovrynSwapNetworkAddress;
-    address public docTokenAddress;
     
+    /**
+     * @dev triggered after liquidity is added
+     *
+     * @param  _provider           liquidity provider
+     * @param  _reserveAmount      provided reserve token amount
+     * @param  _poolTokenAmount    minted pool token amount
+     */
+    event LiquidityAdded(
+        address indexed _provider,
+        uint256 indexed _reserveAmount,
+        uint256 indexed _poolTokenAmount
+    );
+
+    /**
+     * @dev triggered after liquidity is removed
+     *
+     * @param  _provider           liquidity provider
+     * @param  _reserveAmount      added reserve token amount
+     * @param  _poolTokenAmount    burned pool token amount
+     */
+    event LiquidityRemoved(
+        address indexed _provider,
+        uint256 indexed _reserveAmount,
+        uint256 indexed _poolTokenAmount
+    );
+
+    /**
+     * @dev triggered after liquidity is removed
+     *
+     * @param _beneficiary                 account that will receive the conversion result or 0x0 to send the result to the sender account
+     * @param _sourceTokenAmount           amount to convert from, in the source token
+     * @param _targetTokenAmount           amount of tokens received from the conversion, in the target token
+     * @param _sovrynSwapNetworkAddress    address of SovrynSwapNetwork contract
+     * @param _path                        conversion path between two tokens in the network
+     */
+    event TokenConverted(
+        address indexed _beneficiary,
+        uint256 indexed _sourceTokenAmount,
+        uint256 indexed _targetTokenAmount,
+        address _sovrynSwapNetworkAddress,
+        IERC20Token[] _path
+    );
+
     /**
      * @dev To check if ddress is contract address 
      */
@@ -31,31 +73,9 @@ contract RBTCWrapperProxy{
         _;
     }
     
-    constructor (
-        address _wrbtcTokenAddress, 
-        address _liquidityPoolV2ConverterAddress, 
-        address _poolTokenAddress,
-        address _sovrynSwapNetworkAddress,
-        address _docTokenAddress
-        ) 
-        public 
-        checkAddress(_wrbtcTokenAddress)
-        checkAddress(_liquidityPoolV2ConverterAddress)
-        checkAddress(_poolTokenAddress)
-        checkAddress(_sovrynSwapNetworkAddress)
-        checkAddress(_docTokenAddress)
-        {
-            wrbtcTokenAddress = _wrbtcTokenAddress;
-            liquidityPoolV2ConverterAddress = _liquidityPoolV2ConverterAddress;
-            poolTokenAddress = _poolTokenAddress;
-            sovrynSwapNetworkAddress = _sovrynSwapNetworkAddress;
-            docTokenAddress = _docTokenAddress;
-
-            wrbtc = IWrbtcERC20(wrbtcTokenAddress);
-            liquidityPoolV2Converter = ILiquidityPoolV2Converter(liquidityPoolV2ConverterAddress);
-            poolToken = ISmartToken(poolTokenAddress);
-            sovrynSwapNetwork = ISovrynSwapNetwork(sovrynSwapNetworkAddress);
-            docToken = IERC20Token(docTokenAddress);
+    constructor(address _wrbtcTokenAddress) public checkAddress(_wrbtcTokenAddress) {
+        wrbtcTokenAddress = _wrbtcTokenAddress;
+        wrbtc = IWrbtcERC20(wrbtcTokenAddress);        
     }
 
     function() external payable { }
@@ -65,27 +85,47 @@ contract RBTCWrapperProxy{
      * The process:
      * 1.Accepts RBTC
      * 2.Sends RBTC to WRBTC contract in order to wrap RBTC to WRBTC
-     * 3.Calls 'addLiquidity' on LiquidityPoolConverterV2 contract
+     * 3.Calls 'addLiquidity' on LiquidityPoolConverter contract
      * 4.Transfers pool tokens to user
      * 
-     * @param _amount       amount of liquidity to add
-     * @param _minReturn    minimum return-amount of reserve tokens
+     * @param _liquidityPoolConverterAddress    address of LiquidityPoolConverter contract
+     * @param _poolTokenAddress                 address of pool token
+     * @param _amount                           amount of liquidity to add
+     * @param _minReturn                        minimum return-amount of reserve tokens
      *
      * @return amount of pool tokens minted
      */
-    function addLiquidity(uint256 _amount, uint256 _minReturn) payable public returns(uint256) {
+    function addLiquidity(
+        address _liquidityPoolConverterAddress, 
+        address _poolTokenAddress, 
+        uint256 _amount, 
+        uint256 _minReturn
+    )  
+        public
+        payable
+        checkAddress(_liquidityPoolConverterAddress)
+        checkAddress(_poolTokenAddress) 
+        returns(uint256) 
+    {
         require(_amount == msg.value, "The provided amount should be identical to msg.value");
+
+        liquidityPoolConverterAddress = _liquidityPoolConverterAddress;
+        poolTokenAddress = _poolTokenAddress;
+        liquidityPoolConverter = ILiquidityPoolV2Converter(liquidityPoolConverterAddress);
+        poolToken = ISmartToken(poolTokenAddress);
 
         (bool successOfDeposit, ) = wrbtcTokenAddress.call.value(_amount)(abi.encodeWithSignature("deposit()"));
         require(successOfDeposit);
 
-        bool successOfApprove = wrbtc.approve(liquidityPoolV2ConverterAddress, _amount);
+        bool successOfApprove = wrbtc.approve(liquidityPoolConverterAddress, _amount);
         require(successOfApprove);
 
-        uint256 poolTokenAmount = liquidityPoolV2Converter.addLiquidity(IERC20Token(wrbtcTokenAddress), _amount, _minReturn);
+        uint256 poolTokenAmount = liquidityPoolConverter.addLiquidity(IERC20Token(wrbtcTokenAddress), _amount, _minReturn);
         
         bool successOfTransfer = poolToken.transfer(msg.sender, poolTokenAmount);
         require(successOfTransfer);
+
+        emit LiquidityAdded(msg.sender, _amount, poolTokenAmount);
 
         return poolTokenAmount;
     }
@@ -97,40 +137,74 @@ contract RBTCWrapperProxy{
      * @dev  
      * The process:     
      * 1.Transfers pool tokens to this contract
-     * 2.Calls 'removeLiquidity' on LiquidityPoolConverterV2 contract
+     * 2.Calls 'removeLiquidity' on LiquidityPoolConverter contract
      * 3.Calls 'withdraw' on WRBTC contract in order to unwrap WRBTC to RBTC
      * 4.Sneds RBTC to user
      * 
-     * @param _amount       amount of pool tokens to burn
-     * @param _minReturn    minimum return-amount of reserve tokens
+     * @param _liquidityPoolConverterAddress    address of LiquidityPoolConverter contract
+     * @param _poolTokenAddress                 address of pool token
+     * @param _amount                           amount of pool tokens to burn
+     * @param _minReturn                        minimum return-amount of reserve tokens
      * 
-     * @return amount of WRBTC unwrapped to RBTC
+     * @return amount of liquidity removed also WRBTC unwrapped to RBTC
      */
-    function removeLiquidity(uint256 _amount, uint256 _minReturn) public returns(uint256) {
+    function removeLiquidity(
+        address _liquidityPoolConverterAddress, 
+        address _poolTokenAddress, 
+        uint256 _amount, 
+        uint256 _minReturn
+    )   
+        public 
+        checkAddress(_liquidityPoolConverterAddress)
+        checkAddress(_poolTokenAddress) 
+        returns(uint256) 
+    {
+        liquidityPoolConverterAddress = _liquidityPoolConverterAddress;
+        poolTokenAddress = _poolTokenAddress;
+        liquidityPoolConverter = ILiquidityPoolV2Converter(liquidityPoolConverterAddress);
+        poolToken = ISmartToken(poolTokenAddress);
+
         bool successOfTransferFrom = poolToken.transferFrom(msg.sender, address(this), _amount);
         require(successOfTransferFrom);
 
-        uint256 reserveAmount = liquidityPoolV2Converter.removeLiquidity(ISmartToken(poolTokenAddress), _amount, _minReturn);
+        uint256 reserveAmount = liquidityPoolConverter.removeLiquidity(poolToken, _amount, _minReturn);
         
         wrbtc.withdraw(reserveAmount);
         
         msg.sender.transfer(reserveAmount);
+
+        emit LiquidityRemoved(msg.sender, reserveAmount, _amount);
 
         return reserveAmount;
     }
     
     /**
      * @notice
-     * Before calling this function to swap DoC to RBTC, users need approve this contract to be able to spend or transfer their DoC tokens
-     * 
-     * @param _path         conversion path between two tokens in the network
-     * @param _amount       amount to convert from, in the source token
-     * @param _minReturn    if the conversion results in an amount smaller than the minimum return - it is cancelled, must be greater than zero
+     * Before calling this function to swap token to RBTC, users need approve this contract to be able to spend or transfer their tokens
+     *
+     * @param _sovrynSwapNetworkAddress    address of SovrynSwapNetwork contract
+     * @param _path                        conversion path between two tokens in the network
+     * @param _amount                      amount to convert from, in the source token
+     * @param _minReturn                   if the conversion results in an amount smaller than the minimum return - it is cancelled, must be greater than zero
      * 
      * @return amount of tokens received from the conversion
      */
-    function convertByPath(IERC20Token[] memory _path, uint256 _amount, uint256 _minReturn) public payable returns(uint256) {
+    function convertByPath(
+        address _sovrynSwapNetworkAddress,
+        IERC20Token[] memory _path, 
+        uint256 _amount, 
+        uint256 _minReturn
+    ) 
+        public 
+        payable
+        checkAddress(_sovrynSwapNetworkAddress)
+        returns(uint256) 
+    {    
+        sovrynSwapNetworkAddress = _sovrynSwapNetworkAddress;
+        sovrynSwapNetwork = ISovrynSwapNetwork(sovrynSwapNetworkAddress);
+  
         if (msg.value != 0) {
+            require(_path[0] == IERC20Token(wrbtcTokenAddress), "value may only be sent for WRBTC transfers");
             require(_amount == msg.value, "The provided amount should be identical to msg.value");
 
             (bool successOfDeposit, ) = wrbtcTokenAddress.call.value(_amount)(abi.encodeWithSignature("deposit()"));
@@ -139,26 +213,32 @@ contract RBTCWrapperProxy{
             bool successOfApprove = wrbtc.approve(sovrynSwapNetworkAddress, _amount);
             require(successOfApprove);
 
-            uint256 amount = sovrynSwapNetwork.convertByPath(_path, _amount, _minReturn, msg.sender, address(0), 0);
+            uint256 _targetTokenAmount = sovrynSwapNetwork.convertByPath(_path, _amount, _minReturn, msg.sender, address(0), 0);
 
-            return amount;
+            emit TokenConverted(msg.sender, _amount, _targetTokenAmount, sovrynSwapNetworkAddress, _path);
+
+            return _targetTokenAmount;
         }
         else {
-            if (_path[0] == docToken) {
-                bool successOfTransferFrom = docToken.transferFrom(msg.sender, address(this), _amount);
-                require(successOfTransferFrom);
+            require(_path[_path.length-1] == IERC20Token(wrbtcTokenAddress), "It only could be swapped to WRBTC");
+            
+            token = IERC20Token(_path[0]);
 
-                bool successOfApprove = docToken.approve(sovrynSwapNetworkAddress, _amount);
-                require(successOfApprove);
+            bool successOfTransferFrom = token.transferFrom(msg.sender, address(this), _amount);
+            require(successOfTransferFrom);
 
-                uint256 amount = sovrynSwapNetwork.convertByPath(_path, _amount, _minReturn, address(this), address(0), 0);
+            bool successOfApprove = token.approve(sovrynSwapNetworkAddress, _amount);
+            require(successOfApprove);
+                 
+            uint256 _targetTokenAmount = sovrynSwapNetwork.convertByPath(_path, _amount, _minReturn, address(this), address(0), 0);
 
-                wrbtc.withdraw(amount);
+            wrbtc.withdraw(_targetTokenAmount);
 
-                msg.sender.transfer(amount);
+            msg.sender.transfer(_targetTokenAmount);
 
-                return amount;
-            }
+            emit TokenConverted(msg.sender, _amount, _targetTokenAmount, sovrynSwapNetworkAddress, _path);
+
+            return _targetTokenAmount;
         }        
     }
 
---
+++ solidity/contracts/wrapperproxy/test/testRBTCWrapperProxy.js
@@ -0,0 +1,149 @@
+require('@openzeppelin/test-helpers/configure')({
+    provider: web3.currentProvider,
+    singletons: {
+      abstraction: 'truffle',
+    },
+  });
+
+const assert = require('chai').assert;
+const { expectRevert, expectEvent} = require('@openzeppelin/test-helpers');
+const { before } = require('lodash');
+
+const RBTCWrapperProxy = artifacts.require("../contracts/RBTCWrapperProxy.sol");
+const IERC20Token = artifacts.require("../contracts/interfaces/IERC20Token.sol");
+const ISmartToken = artifacts.require("../contracts/interfaces/ISmartToken.sol");
+const ISovrynSwapNetwork = artifacts.require("../contracts/interfaces/ISovrynSwapNetwork.sol");
+const ILiquidityPoolV2Converter = artifacts.require("../contracts/interfaces/ILiquidityPoolV2Converter.sol");
+
+// Update following addresses to test on RSK testnet
+/*
+const liquidityPoolV2ConverterAddress = '0x3ED5C55D08F75488736fb4A2e512698E71251cf0';
+const poolTokenAddress = '0x7F433CC76298bB5099c15C1C7C8f2e89A8370111';
+const docTokenAddress = '0xCB46c0ddc60D18eFEB0E586C17Af6ea36452Dae0';
+const sovrynSwapNetworkAddress = '0x61172B53423E205a399640e5283e51FE60EC2256';
+const wrbtcAddress = '0x69FE5cEC81D5eF92600c1A0dB1F11986AB3758Ab';
+*/
+
+// Update following addresses to test locally
+/*
+const liquidityPoolV2ConverterAddress = '0xaad5B2802FDBB33200F62A30CfD3Fce4FC5dDfBA';
+const docTokenAddress = '0x3194cBDC3dbcd3E11a07892e7bA5c3394048Cc87';
+const sovrynSwapNetworkAddress = '0x64B334435888bb5E44D890a7B319981c4Bb4B47d';
+const wrbtcAddress = '0x602C71e4DAC47a042Ee7f46E0aee17F94A3bA0B6';
+*/
+
+contract('RBTCWrapperProxy', async (accounts) => {
+
+    let rbtcWrapperProxy;
+    let liquidityPoolV2Converter;
+    let poolTokenAddress;
+    let poolToken;
+    let docToken;
+    let sovrynSwapNetwork;
+
+    beforeEach(async () => {
+        rbtcWrapperProxy = await RBTCWrapperProxy.deployed();
+        liquidityPoolV2Converter = await ILiquidityPoolV2Converter.at(liquidityPoolV2ConverterAddress);
+        poolTokenAddress = await liquidityPoolV2Converter.poolToken(wrbtcAddress);
+        poolToken = await ISmartToken.at(poolTokenAddress);
+        docToken = await IERC20Token.at(docTokenAddress);
+        sovrynSwapNetwork = await ISovrynSwapNetwork.at(sovrynSwapNetworkAddress);
+    });
+
+    it('verifies that users could send RBTC and then add liquidity to get pool token', async () => {
+        var rbtcAmountBefore = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
+        var poolTokenAmountBefore = web3.utils.fromWei(await poolToken.balanceOf(accounts[0]));
+        console.log('The account address:', accounts[0]);
+        console.log('The amount of RBTC of the account:', rbtcAmountBefore);
+        console.log('The amount of pool token of the account:', poolTokenAmountBefore);
+
+        var result = await rbtcWrapperProxy.addLiquidity(liquidityPoolV2ConverterAddress, poolToken.address, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address, value:1e16});
+        console.log('The amount of RBTC of the account after sending 0.01 RBTC:', web3.utils.fromWei(await web3.eth.getBalance(accounts[0])));
+        console.log('The amount of pool token of the account after sending 0.01 RBTC:', web3.utils.fromWei(await poolToken.balanceOf(accounts[0])));
+
+        var gasCost = result.receipt.gasUsed * await web3.eth.getGasPrice();
+        var addedPoolTokenAmount = web3.utils.fromWei(result.logs[0].args._poolTokenAmount);
+        assert.equal(await web3.eth.getBalance(accounts[0]), web3.utils.toWei(rbtcAmountBefore) - gasCost - 1e16, "Wrong RBTC balance");
+        assert.equal(web3.utils.BN(await poolToken.balanceOf(accounts[0])).toString(), parseInt(web3.utils.toWei(poolTokenAmountBefore)) + parseInt(web3.utils.toWei(addedPoolTokenAmount)), "Wrong pool token balance");
+        await expectEvent(result.receipt, "LiquidityAdded", {_provider:accounts[0], _reserveAmount:web3.utils.toBN(1e16), _poolTokenAmount:web3.utils.toWei(addedPoolTokenAmount)});
+    });
+
+    it('verifies that users could remove liquidity to burn pool token and then get RBTC', async () => {
+        console.log('The account address:', accounts[0]);
+        console.log('The amount of RBTC of the account:', rbtcAmountBefore);
+        console.log('The amount of pool token of the account:', poolTokenAmountBefore);
+        
+        await poolToken.approve(RBTCWrapperProxy.address, web3.utils.toBN(1e16), {from:accounts[0]});
+        console.log('The allowance of pool token that the RBTCWrapperProxy smart contract could spend:', web3.utils.fromWei(await poolToken.allowance(accounts[0], RBTCWrapperProxy.address)));
+        var rbtcAmountBefore = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
+        var poolTokenAmountBefore = web3.utils.fromWei(await poolToken.balanceOf(accounts[0]));
+        
+        var result = await rbtcWrapperProxy.removeLiquidity(liquidityPoolV2ConverterAddress, poolToken.address, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address});
+        console.log('The amount of RBTC of the account after burning 0.01 pool token:', web3.utils.fromWei(await web3.eth.getBalance(accounts[0])));
+        console.log('The amount of pool token of the account after burning 0.01 pool token:', web3.utils.fromWei(await poolToken.balanceOf(accounts[0])));
+
+        var gasCost = result.receipt.gasUsed * await web3.eth.getGasPrice();
+        var addedRBTCAmount = web3.utils.BN(result.logs[0].args._reserveAmount).toString();
+        assert.equal(await web3.eth.getBalance(accounts[0]), parseInt(web3.utils.toWei(rbtcAmountBefore)) + parseInt(addedRBTCAmount) - gasCost, "Wrong RBTC balance");
+        assert.equal(await poolToken.balanceOf(accounts[0]), web3.utils.toWei(poolTokenAmountBefore) - 1e16, "Wrong pool token balance");
+        await expectEvent(result.receipt, "LiquidityRemoved", {_provider:accounts[0], _reserveAmount:addedRBTCAmount, _poolTokenAmount:web3.utils.toBN(1e16)});
+    });
+    
+    it('verifies that users could send RBTC and then swap it to DoC', async () => {
+        var rbtcAmountBefore = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
+        var docTokenAmountBefore = web3.utils.fromWei((await docToken.balanceOf(accounts[0])));
+        console.log('The account address:', accounts[0]);
+        console.log('The amount of RBTC of the account:', rbtcAmountBefore);
+        console.log('The amount of DoC of the account:', docTokenAmountBefore);
+
+        var pathWRBTCToDoC = await sovrynSwapNetwork.conversionPath(wrbtcAddress, docTokenAddress);
+        var result = await rbtcWrapperProxy.convertByPath(sovrynSwapNetworkAddress, pathWRBTCToDoC, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address, value:1e16});
+        console.log('The amount of RBTC of the account after sending 0.01 RBTC:', web3.utils.fromWei(await web3.eth.getBalance(accounts[0])));
+        console.log('The amount of DoC of the account after sending 0.01 RBTC:', web3.utils.fromWei((await docToken.balanceOf(accounts[0]))));
+        
+        var gasCost = result.receipt.gasUsed * await web3.eth.getGasPrice();
+        var addedDoCTokenAmount = web3.utils.BN(result.logs[0].args._targetTokenAmount).toString();
+        assert.equal(await web3.eth.getBalance(accounts[0]), parseInt(web3.utils.toWei(rbtcAmountBefore)) - gasCost - 1e16, "Wrong RBTC balance");
+        assert.equal(web3.utils.BN(await docToken.balanceOf(accounts[0])).toString(), parseInt(web3.utils.toWei(docTokenAmountBefore)) + parseInt(addedDoCTokenAmount), "Wrong DoC token balance");
+        await expectEvent(result.receipt, "TokenConverted", {_beneficiary:accounts[0], _sourceTokenAmount:web3.utils.toBN(1e16), _targetTokenAmount:addedDoCTokenAmount, _sovrynSwapNetworkAddress:sovrynSwapNetworkAddress, _path:pathWRBTCToDoC});
+    });
+
+    it('verifies that users could send DoC and then swap it to RBTC', async () => {
+        console.log('The account address:', accounts[0]);
+        console.log('The amount of RBTC of the account:', rbtcAmountBefore);
+        console.log('The amount of DoC of the account:', docTokenAmountBefore);
+
+        await docToken.approve(RBTCWrapperProxy.address, web3.utils.toBN(1e20), {from:accounts[0]});
+        console.log('The allowance of DoC that the RBTCWrapperProxy smart contract could spend:', web3.utils.fromWei(((await docToken.allowance(accounts[0], RBTCWrapperProxy.address))).toString()));
+        var rbtcAmountBefore = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
+        var docTokenAmountBefore = web3.utils.fromWei((await docToken.balanceOf(accounts[0])));
+        
+        var pathDoCToWRBTC = await sovrynSwapNetwork.conversionPath(docTokenAddress, wrbtcAddress);
+        var rbtcAmountBefore = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
+        var result = await rbtcWrapperProxy.convertByPath(sovrynSwapNetworkAddress, pathDoCToWRBTC, web3.utils.toBN(1e20), 1, {from:accounts[0], to:RBTCWrapperProxy.address});
+        console.log('The amount of RBTC of the account after sending 100 DoC:', web3.utils.fromWei(await web3.eth.getBalance(accounts[0])));
+        console.log('The amount of DoC of the account after sending 100 DoC:', web3.utils.fromWei((await docToken.balanceOf(accounts[0]))));
+
+        var gasCost = result.receipt.gasUsed * await web3.eth.getGasPrice();
+        var addedRBTCAmount = web3.utils.BN(result.logs[0].args._targetTokenAmount).toString();
+        assert.equal(await web3.eth.getBalance(accounts[0]), parseInt(web3.utils.toWei(rbtcAmountBefore)) + parseInt(addedRBTCAmount) - gasCost, "Wrong RBTC balance");
+        assert.equal(web3.utils.BN(await docToken.balanceOf(accounts[0])).toString(), parseInt(web3.utils.toWei(docTokenAmountBefore)) - 1e20, "Wrong DoC token balance");
+        await expectEvent(result.receipt, "TokenConverted", {_beneficiary:accounts[0], _sourceTokenAmount:web3.utils.toBN(1e20), _targetTokenAmount:addedRBTCAmount, _sovrynSwapNetworkAddress:sovrynSwapNetworkAddress, _path:pathDoCToWRBTC});
+    });
+
+    it('should revert when calling the addLIquidity() without sending RBTC', async () => {
+        await expectRevert.unspecified(rbtcWrapperProxy.addLiquidity(liquidityPoolV2ConverterAddress, poolToken.address, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address}), "No RBTC");
+    });
+
+    it('should revert when amount param not identical to msg.value to call addLiquidity()', async () => {
+        await expectRevert.unspecified(rbtcWrapperProxy.addLiquidity(liquidityPoolV2ConverterAddress, poolToken.address, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address, value:1e8}), "The provided amount not identical to msg.value");
+    });
+    
+    it('should revert when passing wrong path param to convertByPath()', async () => {
+        var pathWRBTCToDoC = await sovrynSwapNetwork.conversionPath(wrbtcAddress, docTokenAddress);
+        var pathDoCToWRBTC = await sovrynSwapNetwork.conversionPath(docTokenAddress, wrbtcAddress);
+        await expectRevert.unspecified(rbtcWrapperProxy.convertByPath(sovrynSwapNetworkAddress, pathDoCToWRBTC, web3.utils.toBN(1e16), 1, {from:accounts[0], to:RBTCWrapperProxy.address, value:1e16}), "Wrong path param");
+        await expectRevert.unspecified(rbtcWrapperProxy.convertByPath(sovrynSwapNetworkAddress, pathWRBTCToDoC, web3.utils.toBN(1e20), 1, {from:accounts[0], to:RBTCWrapperProxy.address}), "Wrong path param");
+    });
+
+});
