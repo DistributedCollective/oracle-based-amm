c92cc8e25cb6c4dda14b4cfbe1f133832a298d1a
Tyrone Johnson <tjcloa@gmail.com>
2022-08-04 18:37:18 +0300

    upd: truffle-config & wrapper proxy deployment script

--- rbtcwrapperproxy/migrations/2_deploy_RBTCWrapperProxy.js
+++ rbtcwrapperproxy/migrations/2_deploy_RBTCWrapperProxy.js
@@ -12,23 +12,31 @@ const getSOVConfig = () => {
 };
 
 module.exports = function (deployer, network) {
-	if(network == "development"){
-		console.log(getConfig()["SUSD"].addr)
+	if (network == "development") {
+		console.log(getConfig()["SUSD"].addr);
 
-		return deployer.deploy(LoanToken, getConfig()["SUSD"].addr).then(function() {
-			return deployer.deploy(LiquidityMining, getSOVConfig()["SOV"].addr).then(function() {
-				return deployer.deploy(RBTCWrapperProxy, getConfig()["RBTC"].addr, getConfig()["sovrynSwapNetwork"].addr, getConfig()["contractRegistry"].addr, LiquidityMining.address);
+		return deployer.deploy(LoanToken, getConfig()["SUSD"].addr).then(function () {
+			return deployer.deploy(LiquidityMining, getSOVConfig()["SOV"].addr).then(function () {
+				return deployer.deploy(
+					RBTCWrapperProxy,
+					getConfig()["RBTC"].addr,
+					getConfig()["sovrynSwapNetwork"].addr,
+					getConfig()["contractRegistry"].addr,
+					LiquidityMining.address
+				);
 			});
 		});
-	}
-		
-	else{
-		liquidityMiningAddress = '0xe28aEbA913c34EC8F10DF0D9C92D2Aa27545870e';
-		wrbtcAddress = '0x69FE5cEC81D5eF92600c1A0dB1F11986AB3758Ab';
-		swapNetworkAddress = '0x61172B53423E205a399640e5283e51FE60EC2256';
-		contractRegistry = '0x0E7CcF6A67e614B507Aa524572F72C7e5Dec23CB';
+	} else if (network == "testnet") {
+		wrbtcAddress = "0x69FE5cEC81D5eF92600c1A0dB1F11986AB3758Ab";
+		swapNetworkAddress = "0x61172B53423E205a399640e5283e51FE60EC2256";
+		contractRegistry = "0x0E7CcF6A67e614B507Aa524572F72C7e5Dec23CB";
+		liquidityMiningAddress = "0xe28aEbA913c34EC8F10DF0D9C92D2Aa27545870e";
 		return deployer.deploy(RBTCWrapperProxy, wrbtcAddress, swapNetworkAddress, contractRegistry, liquidityMiningAddress);
-
-	}
-
+	} else if (network == "mainnet") {
+		wrbtcAddress = "0x542fda317318ebf1d3deaf76e0b632741a7e677d";
+		swapNetworkAddress = "0x98aCE08D2b759a265ae326F010496bcD63C15afc";
+		contractRegistry = "0x46EBC03EF2277308BdB106a73d11c65109C4B89B";
+		liquidityMiningAddress = "0xf730af26e87D9F55E46A6C447ED2235C385E55e0";
+		return deployer.deploy(RBTCWrapperProxy, wrbtcAddress, swapNetworkAddress, contractRegistry, liquidityMiningAddress);
+	} else console.log("Not deployed: unsupported network.");
 };
--- rbtcwrapperproxy/truffle-config.js
+++ rbtcwrapperproxy/truffle-config.js
@@ -18,12 +18,12 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
-const fs = require('fs');
+const fs = require("fs");
 // const mnemonic = "";
 
-const privateKey = fs.readFileSync('./private-key').toString();
+const privateKey = fs.readFileSync("./private-key").toString();
 
 //Update gas price Testnet
 /* Run this first, to use the result in truffle-config:
@@ -34,97 +34,99 @@ const privateKey = fs.readFileSync('./private-key').toString();
 const gasPriceTestnetRaw = fs.readFileSync(".minimum-gas-price-testnet.json").toString().trim();
 
 const minimumGasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result.minimumGasPrice, 16);
-if (typeof minimumGasPriceTestnet !== 'number' || isNaN(minimumGasPriceTestnet)) {
-  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+if (typeof minimumGasPriceTestnet !== "number" || isNaN(minimumGasPriceTestnet)) {
+	throw new Error("unable to retrieve network gas price from .gas-price-testnet.json");
 }
 console.log("Minimum gas price Testnet: " + minimumGasPriceTestnet);
 
-
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+	/**
+	 * Networks define how you connect to your ethereum client and let you set the
+	 * defaults web3 uses to send transactions. If you don't specify one truffle
+	 * will spin up a development blockchain for you on port 9545 when you
+	 * run `develop` or `test`. You can ask a truffle command to use a specific
+	 * network from the command line, e.g
+	 *
+	 * $ truffle test --network <network-name>
+	 */
 
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    testnet: {
-      provider: () => new HDWalletProvider(privateKey, "wss://testnet.sovryn.app/ws"),
-      network_id: 31,
-      gasPrice: 95000010,
-      skipDryRun: true,
-      networkCheckTimeout: 1e9,
-      timeoutBlocks: 500000
-     },
-    mainnet: {
-      provider: () => new HDWalletProvider(privateKey, "wss://mainnet.sovryn.app/ws"),
-      network_id: 30,
-      gasPrice: 65000010,
-      networkCheckTimeout: 1e9,
-      timeoutBlocks: 500000
-    },
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
+	networks: {
+		// Useful for testing. The `development` name is special - truffle uses it by default
+		// if it's defined here and no other network is specified at the command line.
+		// You should run a client (like ganache-cli, geth or parity) in a separate terminal
+		// tab if you use this network and you must also set the `host`, `port` and `network_id`
+		// options below to some value.
+		//
+		testnet: {
+			//provider: () => new HDWalletProvider(privateKey, "wss://testnet.sovryn.app/websocket"),
+			provider: () => new HDWalletProvider(privateKey, "https://testnet.sovryn.app/rpc"),
+			network_id: 31,
+			gasPrice: 95000010,
+			skipDryRun: true,
+			networkCheckTimeout: 1e9,
+			timeoutBlocks: 500000,
+		},
+		mainnet: {
+			//provider: () => new HDWalletProvider(privateKey, "wss://mainnet.sovryn.app/ws"),
+			//provider: () => new HDWalletProvider(privateKey, "wss://mainnet.sovryn.app/websocket"),
+			provider: () => new HDWalletProvider(privateKey, "https://testnet.sovryn.app/rpc"),
+			network_id: 30,
+			gasPrice: 65000010,
+			networkCheckTimeout: 1e9,
+			timeoutBlocks: 500000,
+		},
+		development: {
+			host: "127.0.0.1", // Localhost (default: none)
+			port: 8545, // Standard Ethereum port (default: none)
+			network_id: "*", // Any network (default: none)
+		},
 
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
+		// Another network with more advanced options...
+		// advanced: {
+		// port: 8777,             // Custom port
+		// network_id: 1342,       // Custom network
+		// gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+		// gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+		// from: <address>,        // Account to send txs from (default: accounts[0])
+		// websockets: true        // Enable EventEmitter interface for web3 (default: false)
+		// },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+		// Useful for deploying to a public network.
+		// NB: It's important to wrap the provider as a function.
+		// ropsten: {
+		// provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+		// network_id: 3,       // Ropsten's id
+		// gas: 5500000,        // Ropsten has a lower block limit than mainnet
+		// confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+		// timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+		// skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+		// },
 
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+		// Useful for private networks
+		// private: {
+		// provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+		// network_id: 2111,   // This network is yours, in the cloud.
+		// production: true    // Treats this network as if it was a public net. (default: false)
+		// }
+	},
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		// timeout: 100000
+	},
 
-  // Configure your compilers
-  compilers: {
-    solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
-  }
-}
+	// Configure your compilers
+	compilers: {
+		solc: {
+			// version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+			// docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+			// settings: {          // See the solidity docs for advice about optimization and evmVersion
+			//  optimizer: {
+			//    enabled: false,
+			//    runs: 200
+			//  },
+			//  evmVersion: "byzantium"
+			// }
+		},
+	},
+};
